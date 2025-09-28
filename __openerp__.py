cd4256d085b00ace32492665ef44be244e1fe072
Anthony Escobar <anthonyrabocse@gmail.com>
2025-09-28 17:08:45 -0600

    feat(module): actualizacion detallada de posibles errores en modulo para su compatibilidad con odoo8

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 {
   'name': 'Modulo: administracion estudiantil',
-  'version': '1.0',
+  'version': '1.2',
   'author': 'Anthony Escobar / AE-Solutions',
   'category': 'Education',
   'depends': ['base','web'],

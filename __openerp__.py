8449b2dd1b3f36b59ae7a977b3527f78e1828b8a
Anthony Escobar <anthonyrabocse@gmail.com>
2025-09-29 10:30:31 -0600

    fix(model-views): correcciones apra vistas y modelos

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 {
   'name': 'Modulo: administracion estudiantil',
-  'version': '1.2.7',
+  'version': '1.2.8',
   'author': 'Anthony Escobar / AE-Solutions',
   'category': 'Education',
   'depends': ['base','web'],
